71bbc4c7f7a443ed5d5d75a63e7a9b009c679be3
dani <daniel_sephirot@hotmail.com>
2016-11-22 12:15:14 +0100

    include fragment_login

--- app/src/main/java/utad/actividad3android/MainActivity.java
+++ app/src/main/java/utad/actividad3android/MainActivity.java
@@ -1,14 +1,26 @@
 package utad.actividad3android;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+     TextFragment testFragment;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FragmentManager fm=getSupportFragmentManager();
+        testFragment=(TextFragment)fm.findFragmentById(R.id.MiFragmento);
+        FragmentTransaction transaction=fm.beginTransaction();
+        transaction.hide(testFragment);
+        transaction.show(testFragment);
+        transaction.commit();
+
+
 
 
     }
--- libreria/src/main/java/utad/libreria/QBAdmin.java
+++ libreria/src/main/java/utad/libreria/QBAdmin.java
@@ -5,4 +5,6 @@ package utad.libreria;
  */
 public class QBAdmin {
 
+
+
 }
