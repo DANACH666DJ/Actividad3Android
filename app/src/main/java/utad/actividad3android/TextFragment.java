f744ad046dad030538e5e10c8b65074ccd1ec31b
dani <daniel_sephirot@hotmail.com>
2016-11-22 10:28:30 +0100

    creamos el primer fragment

---
+++ app/src/main/java/utad/actividad3android/TextFragment.java
@@ -0,0 +1,38 @@
+package utad.actividad3android;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TextFragment extends Fragment {
+
+    public Button btn;
+
+
+    public TextFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        View v=inflater.inflate(R.layout.fragment_text, container, false);
+        btn=(Button)v.findViewById(R.id.button);
+        return v;
+
+
+
+    }
+
+}
