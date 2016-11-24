b19702d0332fd456a336834f5aedb144342d0c5a
dani <daniel_sephirot@hotmail.com>
2016-11-24 12:50:30 +0100

    creamos un fragment para las imagenes y le metemos las imagenes

--- app/src/main/java/utad/actividad3android/ImagenesFragment.java
+++ app/src/main/java/utad/actividad3android/ImagenesFragment.java
@@ -6,28 +6,30 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class LoginFragment extends Fragment {
-    public TextView texto;
+public class ImagenesFragment extends Fragment {
+    public ImageView imagen;
 
-
-
-    public LoginFragment() {
+    public ImagenesFragment() {
         // Required empty public constructor
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v=inflater.inflate(R.layout.fragment_login, container, false);
-        texto=(TextView) v.findViewById(R.id.textoLogin);
+        View v=inflater.inflate(R.layout.fragment_imagenes, container, false);
+        imagen=(ImageView) v.findViewById(R.id.imagen1);
         return v;
+
+
 
     }
 
