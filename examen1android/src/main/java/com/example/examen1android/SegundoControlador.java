9d7c9c2d549ab8f3b6f58acb5f69dde46f813aa6
daniel <daniel_sephirot@hotmail.com>
2016-12-20 11:43:48 +0100

    implementamos los botones del fragment categoriaNoticias y creamos un segundo controlador para este segundo activity

---
+++ examen1android/src/main/java/com/example/examen1android/SegundoControlador.java
@@ -0,0 +1,27 @@
+package com.example.examen1android;
+
+import android.view.View;
+
+import utad.libreria.QBAdmin;
+
+/**
+ * Created by asus on 20/12/2016.
+ */
+
+public class SegundoControlador implements View.OnClickListener, View.OnFocusChangeListener{
+    SegundoActivity vista;
+
+    public SegundoControlador(SegundoActivity vista) {
+        this.vista = vista;
+
+
+    }
+    public void onClick(View view) {
+
+    }
+
+    @Override
+    public void onFocusChange(View view, boolean b) {
+
+    }
+}
