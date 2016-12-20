6a850b2b7fc6ea5a871e9d23698197fba95da2c4
daniel <daniel_sephirot@hotmail.com>
2016-12-20 10:24:00 +0100

    pasamos a implementar en el controlador el QBAdmin y QBAdminListener para poder usar todos los metodos de quickblox y poder conectar con quickblox

--- examen1android/src/main/java/com/example/examen1android/Controlador.java
+++ examen1android/src/main/java/com/example/examen1android/Controlador.java
@@ -3,18 +3,27 @@ package com.example.examen1android;
 import android.view.View;
 import android.widget.Button;
 
+import com.quickblox.core.model.QBBaseCustomObject;
+import com.quickblox.users.model.QBUser;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import utad.libreria.QBAdmin;
+import utad.libreria.QBAdminListener;
 
 /**
  * Created by asus on 20/12/2016.
  */
 
-public class Controlador implements View.OnClickListener, View.OnFocusChangeListener{
+public class Controlador implements View.OnClickListener, View.OnFocusChangeListener, QBAdminListener {
     MainActivity vista;
-    
+    QBAdmin qbAdmin;
+
 
     public Controlador(MainActivity vista) {
         this.vista = vista;
+        qbAdmin = new QBAdmin(this, vista);
 
     }
 
@@ -26,6 +35,26 @@ public class Controlador implements View.OnClickListener, View.OnFocusChangeList
 
     @Override
     public void onFocusChange(View view, boolean b) {
+
+    }
+
+    @Override
+    public void logeado(boolean logeado, QBUser user) {
+
+    }
+
+    @Override
+    public void registrado(boolean registrado, QBUser user) {
+
+    }
+
+    @Override
+    public void datosDescargados(ArrayList<QBBaseCustomObject> datos) {
+
+    }
+
+    @Override
+    public void datosTablaIdiomaDescargados(HashMap<Integer, String> p) {
 
     }
 }
