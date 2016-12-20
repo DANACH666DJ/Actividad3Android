c862d4773a3d6ab5d563f1d2ba17e6bd5d4da5fc
daniel <daniel_sephirot@hotmail.com>
2016-12-20 11:54:39 +0100

    implementamos los botones tanto en activity como en su fragment y los instanciamos

--- examen1android/src/main/java/com/example/examen1android/CategoriaNoticias.java
+++ examen1android/src/main/java/com/example/examen1android/CategoriaNoticias.java
@@ -6,12 +6,20 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class CategoriaNoticias extends Fragment {
+    Button btnDeportes ;
+    Button btnTurismo;
+    Button btnTecnologia;
+    Button btnArte;
+    Button btnPolitica;
+    Button btnCine;
 
 
     public CategoriaNoticias() {
@@ -23,7 +31,18 @@ public class CategoriaNoticias extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_categoria_noticias, container, false);
+        View v=inflater.inflate(R.layout.fragment_categoria_noticias, container, false);
+
+        btnDeportes=(Button)v.findViewById(R.id.btnDeportes);
+        btnTurismo=(Button)v.findViewById(R.id.btnTurismo);
+        btnTecnologia=(Button) v.findViewById(R.id.btnTecnologia);
+        btnArte=(Button) v.findViewById(R.id.btnArte);
+        btnPolitica=(Button) v.findViewById(R.id.btnPolitica);
+        btnCine=(Button) v.findViewById(R.id.btnCine);
+
+
+
+        return v;
     }
 
 }
--- examen1android/src/main/java/com/example/examen1android/SegundoActivity.java
+++ examen1android/src/main/java/com/example/examen1android/SegundoActivity.java
@@ -1,13 +1,35 @@
 package com.example.examen1android;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class SegundoActivity extends AppCompatActivity {
+    CategoriaNoticias categNot;
+    Noticias noticias;
+    InformacionNoticia inforNot;
+    Button btnDeportes ;
+    Button btnTurismo;
+    Button btnTecnologia;
+    Button btnArte;
+    Button btnPolitica;
+    Button btnCine;
+
+    FragmentTransaction transaction;
+    SegundoControlador controlador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_segundo);
+        controlador=new SegundoControlador(this);
+        FragmentManager fm=getSupportFragmentManager();
+        categNot=(CategoriaNoticias) fm.findFragmentById(R.id.fragmentCatNoticias);
+        noticias=(Noticias) fm.findFragmentById(R.id.fragmentNoticias);
+        inforNot=(InformacionNoticia) fm.findFragmentById(R.id.fragmentInforNoticia);
+
+
     }
 }
