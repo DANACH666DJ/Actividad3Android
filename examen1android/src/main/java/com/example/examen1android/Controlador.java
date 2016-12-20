b07d2ff492305a12a7074f03203badf9ab56fd1c
daniel <daniel_sephirot@hotmail.com>
2016-12-20 10:10:54 +0100

    creamos un controlador para capturar todos los eventos de nuestri activity y incorporamos al mainActivity todos los botones ,textos,controlador etc

---
+++ examen1android/src/main/java/com/example/examen1android/Controlador.java
@@ -0,0 +1,29 @@
+package com.example.examen1android;
+
+import android.view.View;
+
+import utad.libreria.QBAdmin;
+
+/**
+ * Created by asus on 20/12/2016.
+ */
+
+public class Controlador implements View.OnClickListener, View.OnFocusChangeListener{
+    MainActivity vista;
+
+    public Controlador(MainActivity vista) {
+        this.vista = vista;
+
+    }
+
+
+    @Override
+    public void onClick(View view) {
+
+    }
+
+    @Override
+    public void onFocusChange(View view, boolean b) {
+
+    }
+}
--- examen1android/src/main/java/com/example/examen1android/MainActivity.java
+++ examen1android/src/main/java/com/example/examen1android/MainActivity.java
@@ -1,9 +1,18 @@
 package com.example.examen1android;
 
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Registro registroFragment;
+    Login loginFragment;
+    Button btnLogin ;
+    Button btnRegistro;
+    Button btnOkLogin;
+    Button btnOkRegistro;
+    FragmentTransaction transaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
